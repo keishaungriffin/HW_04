b020ee1c3073caa7f5de9c1c02f042e69e8cd56a
keishaungriffin <113074835+keishaungriffin@users.noreply.github.com>
2022-10-12 19:18:41 -0500

    Update HW_04_Test.py

--- HW_04_Test.py
+++ HW_04_Test.py
@@ -7,14 +7,21 @@ Created on Tue Oct  4 22:32:11 2022
 
 import unittest
 
+import unittest.mock as mock
+
 from HW_04_Keishaun_Griffin_REST_based_APIs import GithubApi
 
 
 class TestGithubAPI(unittest.TestCase):
+    
     def testGithub(self):
-        self.assertEqual(GithubApi('?'), False)
+        with mock.patch('HW_04_Keishaun_Griffin_REST_based_APIs.GithubAPI', create=True) as MockGithub:
+            MockGithub.return_value = False
+            self.assertEqual(MockGithub('?'), False)
     def testGithub2(self):
-        self.assertEqual(GithubApi('keishaungriffin'), True)
-       
+        with mock.patch('HW_04_Keishaun_Griffin_REST_based_APIs.GithubAPI', create=True) as MockGithub:
+            MockGithub.return_value = True
+            self.assertEqual(MockGithub('keishaungriffin'), True)
+            
 if __name__ == '__main__':
     unittest.main()
